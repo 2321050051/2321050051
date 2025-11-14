5c0d101c942cac707e09c74877baa33935b346e0
2321050051 <nduytuan11@gmail.com>
2025-11-14 11:46:01 +0700

    sql

--- Bai7/script.js
+++ Bai7/script.js
@@ -6,29 +6,202 @@ let danhSachPhim = [
         tuoi: 16,
         thoiLuong: 2,
         quocGia: "Việt Nam",
-        poster: "url('img/mai.jpg')",
+        poster: "./mai.jpg",
         theLoai: "Phim chiếu rạp",
+        trailer: "https://www.youtube.com/watch?v=Yz96EBNwMGw"
     },
     {
         id: 2,
+        tenPhim: "Bố Già",
+        namPhatHanh: 2021,
+        tuoi: 16,
+        thoiLuong: 2,
+        quocGia: "Việt Nam",
+        poster: "./bogia.jpg",
+        theLoai: "Phim chiếu rạp",
+        trailer: "https://www.youtube.com/watch?v=jluSu8Rw6YE"
+    },
+    {
+        id: 3,
+        tenPhim: "Chí Phèo (Ngoại Truyện)",
+        namPhatHanh: 2017,
+        tuoi: 10,
+        thoiLuong: 2,
+        quocGia: "Việt Nam",
+        poster: "./chipheo.jpg",
+        theLoai: "Phim chiếu rạp",
+        trailer: "https://www.youtube.com/watch?v=-tMxINnNsjE"
+    },
+    {
+        id: 4,
+        tenPhim: "Con Nhót Mót Chồng",
+        namPhatHanh: 2023,
+        tuoi: 16,
+        thoiLuong: 2,
+        quocGia: "Việt Nam",
+        poster: "./connhotmotchong.jpg",
+        theLoai: "Phim chiếu rạp",
+        trailer: "https://www.youtube.com/watch?v=e7KHOQ-alqY"
+    },
+    {
+        id: 5,
+        tenPhim: "Làm Giàu Với Ma",
+        namPhatHanh: 2024,
+        tuoi: 16,
+        thoiLuong: 2,
+        quocGia: "Việt Nam",
+        poster: "./lamgiauvoima.jpg",
+        theLoai: "Phim chiếu rạp",
+        trailer: "https://www.youtube.com/watch?v=MtZ_hf7tLxk"
+    },
+    {
+        id: 6,
+        tenPhim: "Mưa Đỏ",
+        namPhatHanh: 2024,
+        tuoi: 16,
+        thoiLuong: 2,
+        quocGia: "Việt Nam",
+        poster: "./muado1.jpg",
+        theLoai: "Phim chiếu rạp",
+        trailer: "https://www.youtube.com/watch?v=BD6PoZJdt_M"
+    },
+    {
+        id: 7,
+        tenPhim: "Bẫy Tiền",
+        namPhatHanh: 2025,
+        tuoi: 16,
+        thoiLuong: 2,
+        quocGia: "Việt Nam",
+        poster: "./baytien.jpg",
+        theLoai: "Phim chiếu rạp",
+        trailer: "https://www.youtube.com/watch?v=0wuVwkK-Vsc"
+    },
+    {
+        id: 8,
+        tenPhim: "Bến Phà Xác Sống",
+        namPhatHanh: 2023,
+        tuoi: 16,
+        thoiLuong: 2,
+        quocGia: "Việt Nam",
+        poster: "./benphaxacsong.jpg",
+        theLoai: "Phim chiếu rạp",
+        trailer: "https://www.youtube.com/watch?v=1Jz3e_lq-wg"
+    },
+    {
+        id: 9,
+        tenPhim: "Địa Đạo",
+        namPhatHanh: 2025,
+        tuoi: 16,
+        thoiLuong: 2,
+        quocGia: "Việt Nam",
+        poster: "./diadao.jpg",
+        theLoai: "Phim chiếu rạp",
+        trailer: "https://www.youtube.com/watch?v=xh6IDHjvytU"
+    },
+    {
+        id: 10,
+        tenPhim: "Mắt Biếc",
+        namPhatHanh: 2019,
+        tuoi: 16,
+        thoiLuong: 2,
+        quocGia: "Việt Nam",
+        poster: "./matbiec.jpg",
+        theLoai: "Phim chiếu rạp",
+        trailer: "https://www.youtube.com/watch?v=ITlQ0oU7tDA"
+    },
+    {
+        id: 11,
+        tenPhim: "Mưa Đỏ",
+        namPhatHanh: 2019,
+        tuoi: 16,
+        thoiLuong: 2,
+        quocGia: "Việt Nam",
+        poster: "./muado3.jpeg",
+        theLoai: "Phim chiếu rạp",
+        trailer: "https://www.youtube.com/watch?v=BD6PoZJdt_M"
+    },
+    {
+        id: 12,
+        tenPhim: "Mười Lời Nguyền Trở Lại",
+        namPhatHanh: 2019,
+        tuoi: 16,
+        thoiLuong: 2,
+        quocGia: "Việt Nam",
+        poster: "./muoi.jpg",
+        theLoai: "Phim chiếu rạp",
+        trailer: "https://www.youtube.com/watch?v=FQhA7--t2XE"
+    },
+    {
+        id: 13,
+        tenPhim: "Linh Miếu Quỷ Nhập Tràng",
+        namPhatHanh: 2019,
+        tuoi: 16,
+        thoiLuong: 2,
+        quocGia: "Việt Nam",
+        poster: "./quynhaptrang.jpg",
+        theLoai: "Phim chiếu rạp",
+        trailer: "https://www.youtube.com/watch?v=XsPl7SbL2kg"
+    },
+    {
+        id: 14,
+        tenPhim: "Tử Chiến Trên Không",
+        namPhatHanh: 2019,
+        tuoi: 16,
+        thoiLuong: 2,
+        quocGia: "Việt Nam",
+        poster: "./tuchientrenkhong.jpg",
+        theLoai: "Phim chiếu rạp",
+        trailer: "https://www.youtube.com/watch?v=Q-Zf8KhyS6E"
+    },
+    {
+        id: 15,
+        tenPhim: "Vùng Đất Bị Nguyền Rủa",
+        namPhatHanh: 2019,
+        tuoi: 16,
+        thoiLuong: 2,
+        quocGia: "Việt Nam",
+        poster: "./vungdatbinguyenrua.jpg",
+        theLoai: "Phim chiếu rạp",
+        trailer: "https://www.youtube.com/watch?v=gBsdijgRpl4"
+    },
+    {
+        id: 16,
         tenPhim: "Conan",
         namPhatHanh: 2023,
         tuoi: 10,
         thoiLuong: 1.5,
         quocGia: "Nhật Bản",
-        poster: "url('img/conan.jpg')",
+        poster: "./conan.jpg",
         theLoai: "Phim hoạt hình",
+        trailer: "https://www.youtube.com/watch?v=dz5mN-iIC4g"
     }
 ];
 
 let phimHienTai = danhSachPhim[0];
 
-let banner = document.getElementsByClassName('banner')[0];
+let gioithieuphim = document.getElementsByClassName('banner')[0];
+let trailer = document.getElementById('trailer');
+let tenPhim = document.getElementById('tenPhim');
+let nam = document.getElementById('namPhatHanh');
+let tuoi = document.getElementById('tuoi');
+let thoiLuong = document.getElementById('thoiLuong');
+let quocGia = document.getElementById('quocGia');
+let theLoai = document.getElementById('theLoai');
 
 function chonPhim(idPhim){
     for (let i = 0;i < danhSachPhim.length; i++){
         if (danhSachPhim[i].id == idPhim){
-            banner.style.backgroundImage = danhSachPhim[i].poster;
+             trailer.href = danhSachPhim[i].trailer;
+            gioithieuphim.src = danhSachPhim[i].poster;
+           
+            tenPhim.innerHTML = danhSachPhim[i].tenPhim;
+            nam.innerHTML = danhSachPhim[i].namPhatHanh;
+            tuoi.innerHTML = danhSachPhim[i].tuoi + " +";
+            thoiLuong.innerHTML = danhSachPhim[i].thoiLuong + " tiếng";
+            quocGia.innerHTML = danhSachPhim[i].quocGia;
+            theLoai.innerHTML = danhSachPhim[i].theLoai;
+
+            break;
         }
     }
 }
